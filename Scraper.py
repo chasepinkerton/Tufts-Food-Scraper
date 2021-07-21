2c70032b856792c04c0ee8deae3c8ae37df2c598
Chase Pinkerton <chasepinky@gmail.com>
2021-07-21 19:02:29 -0400

    returns days

--- Scraper.py
+++ Scraper.py
@@ -8,20 +8,20 @@ import pandas as pd
 
 i = 0
 
-month = 5
+month = 7
 month = str(month)
-day = 1
+day = 22
 year = 2021
 year = str(year)
 column_names = ["Dish", "Meal", "Day"]
 df3 = pd.DataFrame(columns = column_names)
 
-while day < 18:
+while day < 23:
     url_date = month + '%2f' + str(day) + '%2f' + year
     date = month + '/' + str(day) + '/' + year
 
     # Get data from menu
-    URL = 'http://menus.tufts.edu/FoodPro%203.1.NET/shortmenu.aspx?sName=TUFTS+DINING&locationNum=11&locationName=Dewick+Dining+Center&naFlag=1&WeeksMenus=This+Week%27s+Menus&myaction=read&dtdate=' + url_date
+    URL = 'https://menus.tufts.edu/FoodPro%203.1.NET/shortmenu.aspx?sName=TUFTS+DINING&locationNum=11&locationName=Dewick2GO&naFlag=1' + url_date
     page = requests.get(URL)
 
     # Convert to parsed html in Soup format
@@ -30,6 +30,7 @@ while day < 18:
     # Filter by class name "shortmenurecipes" and "shortmenumeals" for item/meal
     menu = soup.find_all("div", {"class":["shortmenurecipes", "shortmenumeals"]})
     breakfast = []
+    brunch = []
     lunch = []
     dinner = []
     meal = 0
@@ -41,6 +42,9 @@ while day < 18:
         # print(dish)
         if (dish == "Breakfast"):
             meal = 1
+            continue
+        if (dish == "Brunch"):
+            meal = 4
             continue
         if (dish == "Lunch"):
             meal = 2
@@ -54,7 +58,8 @@ while day < 18:
             lunch.append(dish)
         if (meal == 3):
             dinner.append(dish)
-        
+        if (meal == 4):
+            brunch.append(dish)
 
     df_breakfast = pd.DataFrame()
     df_breakfast['Dish']  = breakfast
@@ -68,14 +73,17 @@ while day < 18:
     df_dinner['Dish']  = dinner
     df_dinner = df_dinner.assign(Meal='Dinner')
     
+    df_brunch = pd.DataFrame()
+    df_brunch['Dish']  = brunch
+    df_brunch = df_brunch.assign(Meal='Brunch')
     
     day = day + 1
     if (day != 1):
-        df2 = pd.concat([df_breakfast, df_lunch, df_dinner])
+        df2 = pd.concat([df_breakfast, df_brunch, df_lunch, df_dinner])
         df2 = df2.assign(Day=date)
         df3 = pd.concat([df2, df3])
     else:
-        df3 = pd.concat([df_breakfast, df_lunch, df_dinner])
+        df3 = pd.concat([df_breakfast, df_brunch, df_lunch, df_dinner])
         df3 = df1.assign(Day=date)    
 
 
--- read_csv.py
+++ read_csv.py
@@ -5,22 +5,31 @@ import pandas as pd
 import os
 
 
-df = pd.read_csv('menu.csv')  
+df = pd.read_csv(('menu.csv'), index_col=0)  
 
 unique_dish = df['Dish'].unique().tolist()
 
 inp = input("What food are you looking for?\n")
 
 matched_dishes = (process.extract(inp, unique_dish, scorer=fuzz.partial_ratio))
+# pprint(matched_dishes)
 
-#Create a dataframe from the tuples
+# Create a dataframe from the tuples
 similarity_sort = pd.DataFrame(matched_dishes, columns=['dish','score_sort'])
 
-high_score_sort = similarity_sort[(similarity_sort['score_sort'] >= 84)]['score_sort'], ascending=False)
+# Only get high matches
+high_score_sort = similarity_sort[(similarity_sort['score_sort'] >= 84)]
 
-
+# Convert to list
 matched_dishes_cutoff = list(high_score_sort.dish)
 
-for dish in matched_dishes_cutoff:
-    # print(df.loc[df['dish'] == dish.text])
-    print(dish)
+# for dish in matched_dishes_cutoff:
+#     # print(df.loc[df['dish'] == dish.text])
+#     print(dish)
+#     # pprint(df.loc[df['Dish'] == dish])
+
+results = df[df['Dish'].isin(matched_dishes_cutoff)]
+
+pprint(results)
+
+
